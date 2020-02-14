8a8e728f43fa43c4fd18cdc98816481d428ee0ef
Alain Martel <alain74martel@gmail.com>
2020-02-14 09:29:18 -0500

    Update Servo8Bit.cpp

--- Servo8Bit.cpp
+++ Servo8Bit.cpp
@@ -521,8 +521,7 @@ void ServoSequencer::setupTimerPrescaler()
             TCCR0B &= ~(1<< CS00); //clear
         #else
             //unsupported clock speed
-            //TODO: find a way to have the compiler stop compiling and bark at the user
-			#error clock speed not supported
+            #error Unsupported clock speed. This code will only operate at 16.5Mhz, 16Mhz, 8Mhz or 1Mhz
         #endif
     #endif
 
@@ -564,8 +563,7 @@ void ServoSequencer::setupTimerPrescaler()
             TCCR1 &= ~(1<< CS10); //clear
         #else
             //unsupported clock speed
-            //TODO: find a way to have the compiler stop compiling and bark at the user
-			#error clock speed not supported
+            #error Unsupported clock speed. This code will only operate at 16.5Mhz, 16Mhz, 8Mhz or 1Mhz
         #endif
     #endif
 }//end setupTimerPrescaler
